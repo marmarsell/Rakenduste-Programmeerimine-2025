73c97018f02dcbd541f9d2000a642c3ab69b5a16
marmarsell <marsell@tlu.ee>
2025-09-17 19:45:00 +0300

    feat: allow changing increment of a counter

--- lesson1/educationalApp/src/components/Counter.tsx
+++ lesson1/educationalApp/src/components/Counter.tsx
@@ -3,14 +3,29 @@ import "../App.css"
 
 function Counter() {
   const [count, setCount] = useState(0)
+  const [increment, setIncrement] = useState(1)
+
+  function incrementChange() {
+    setIncrement(parseInt(document.getElementById("increment-selector").value, 10))
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button onClick={() => setCount(count => count + increment)}>
           count is {count}
         </button>
+        <select name="increment" id="increment-selector" defaultValue={1} onChange={() => incrementChange()}>
+          <option value="100">+100</option>
+          <option value="50">+50</option>
+          <option value="25">+25</option>
+          <option value="1">+1</option>
+          <option value="-1">-1</option>
+          <option value="-25">-25</option>
+          <option value="-50">-50</option>
+          <option value="-100">-100</option>
+        </select>
       </div>
     </>
   )
