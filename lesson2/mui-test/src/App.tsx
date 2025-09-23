b449b54de91e1abd44b00364b58271a7abaf681e
marmarsell <marsell@tlu.ee>
2025-09-23 22:30:29 +0300

    style: added a theme and wrapped entirety of app in it. For whatever reason did nothing

--- lesson2/mui-test/src/App.tsx
+++ lesson2/mui-test/src/App.tsx
@@ -5,19 +5,27 @@ import About from './components/About'
 import Home from './components/Home'
 import { Route, Routes } from 'react-router'
 import Something from './components/Something'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { orange } from '@mui/material/colors'
 
 function App() {
 
+  const theme = createTheme({
+    colorSchemes: orange,
+  })
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='home' element={<Home />} />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Routes>
+          <Route path='home' element={<Home />} />
 
-        <Route path='about' element={<About />} />
+          <Route path='about' element={<About />} />
 
-        <Route path="sumthin'" element={<><Something /><Header/></>} />
-      </Routes>
+          <Route path="sumthin'" element={<><Something /><Header/></>} />
+        </Routes>
+      </ThemeProvider>
     </>
   )
 }
