32fe076de10009e9e56bd93dffe7f9cd28cf30e7
marmarsell <marsell@tlu.ee>
2025-09-25 15:47:47 +0300

    feat: lesson complete

--- lesson3/marsell/backend/controllers/cats.controller.js
+++ lesson3/marsell/backend/controllers/cats.controller.js
@@ -1,6 +1,32 @@
-exports.create = (req, res) => {};
+const cats = [
+  {
+    id: "7d613b93-fa3e-4ef3-a9d2-e09e5ca6e4e6",
+    name: "Meow",
+    createdAt: 1727098800585,
+    updatedAt: null,
+    deleted: false,
+  },
+  {
+    id: "2dc9ce08-d345-4fed-8560-4c6b66fb0836",
+    name: "Kitty",
+    createdAt: 1727098952739,
+    updatedAt: null,
+    deleted: false,
+  },
+];
 
-exports.read = (req, res) => {};
+
+exports.create = (req, res) => {
+    const { name } = req.body
+
+    console.log(name);
+    //console.log(req.body.name)
+    res.sendStatus(200);
+};
+
+exports.read = (req, res) => {
+    res.send(cats);
+};
 
 exports.update = (req, res) => {};
 
--- lesson3/marsell/backend/index.js
+++ lesson3/marsell/backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require("cors")
 
 const catsRoutes = require("./routes/cats.routes");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/cats", catsRoutes);
